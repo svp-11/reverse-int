0937d4648bd63fcfe4ee6b82247d989d46710d93
Valery Smalko <smalko9493@yandex.by>
2021-08-23 16:41:03 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let a = n;
+    if( n < 0) {
+        a=n*(-1);
+    }
+    return Number(String(a).split('').reduceRight((ak, el) => ak+el,''));
+
 }
